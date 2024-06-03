e7114534aa2bd88dfc239e1cd48250a6e767539c
jkky-98 <aal2525@ajou.ac.kr>
2024-06-03 22:52:30 +0900

    arrayList study

---
+++ java-mid2/src/collection/array/ArrayMain2.java
@@ -0,0 +1,36 @@
+package collection.array;
+
+import java.util.Arrays;
+
+public class ArrayMain2 {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println("first in");
+        int newValue = 3;
+        addFirst(arr, newValue);
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println("index(2) in");
+        int index = 2;
+        int value = 4;
+        addAtIndex(arr, index, value);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void addFirst(int[] arr, int newValue) {
+        for (int i = arr.length - 1; i>0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = newValue;
+    }
+
+    private static void addAtIndex(int[] arr, int index, int newValue) {
+        for (int i = arr.length - 1; i > index; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[index] = newValue;
+    }
+}
