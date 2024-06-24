51b3ec3cafde4e4c8d339864fa26512d612f72ed
jkky-98 <aal2525@ajou.ac.kr>
2024-06-24 23:24:12 +0900

    feat : 회원 관리 예제 spring 기본편 스터디 코드

---
+++ core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,43 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig {
+    public MemberService memberService() {
+        return new MemberServiceImpl(MemberRepository());
+    }
+
+    private static MemberRepository MemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(MemberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+}
+
+// Before Refactoring
+//public class AppConfig {
+//    public MemberService memberService() {
+//        return new MemberServiceImpl(new MemoryMemberRepository());
+//    }
+//
+//    public OrderService orderService() {
+//        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+//    }
+//}
+
+
