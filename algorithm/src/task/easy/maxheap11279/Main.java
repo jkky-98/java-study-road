cac421f0709c051195544b18abd967e6036f6420
jkky-98 <aal2525@ajou.ac.kr>
2024-07-16 14:20:28 +0900

    java algorithm study code

--- algorithm/src/task/easy/maxheap11279/Main.java
+++ algorithm/src/task/easy/maxheap11279/Main.java
@@ -1,2 +1,34 @@
-package task.easy.maxheap11279;public class Main {
+package task.easy.maxheap11279;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if (input == 0) {
+                if (heap.isEmpty()) {
+                    System.out.println("0");
+                } else {
+                    Integer poll = heap.poll();
+                    System.out.println(poll);
+                }
+            } else {
+                heap.add(input);
+            }
+        }
+
+
+
+    }
 }
