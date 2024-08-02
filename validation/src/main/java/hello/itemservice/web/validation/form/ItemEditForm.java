16d62a820fa3513ec1325eef297a1f9e7380f6da
jkky-98 <aal2525@ajou.ac.kr>
2024-08-02 13:49:38 +0900

    lab gold4

---
+++ validation/src/main/java/hello/itemservice/web/validation/form/ItemEditForm.java
@@ -0,0 +1,23 @@
+package hello.itemservice.web.validation.form;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Max;
+
+@Data
+public class ItemSaveForm {
+
+    @NotBlank
+    private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
+    private Integer price;
+
+    @Max(value = 9999)
+    @NotNull
+    private Integer quantity;
+}
