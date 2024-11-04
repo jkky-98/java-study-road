a5524862142ac1f3905a39fb2104212a2a8086c1
jkky-98 <aal2525@ajou.ac.kr>
2024-11-04 11:59:50 +0900

    feat: spring boot library management & AutoConfiguration 2 study code

---
+++ boot/external-read/src/main/java/hello/config/MyDataSourceConfigV2.java
@@ -0,0 +1,26 @@
+package hello.config;
+
+import hello.datasource.MyDataSource;
+import hello.datasource.MyDataSourcePropertiesV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@Slf4j
+@EnableConfigurationProperties(MyDataSourcePropertiesV2.class) // 빈으로 등록시켜줌
+@RequiredArgsConstructor
+public class MyDataSourceConfigV2 {
+    private final MyDataSourcePropertiesV2 properties;
+
+    @Bean
+    public MyDataSource dataSource() {
+        return new MyDataSource(
+                properties.getUrl(),
+                properties.getUsername(),
+                properties.getPassword(),
+                properties.getEtc().getMaxConnection(),
+                properties.getEtc().getTimeout(),
+                properties.getEtc().getOptions());
+    }
+}
