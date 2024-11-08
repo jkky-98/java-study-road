7d1c4d6a1abe8f0ceb4e3787acec7f1ffd481196
jkky-98 <aal2525@ajou.ac.kr>
2024-11-08 15:48:44 +0900

    feat: ThreadLocal , Log Tracker study code

---
+++ advanced/src/main/java/hello/advanced/app/v4/OrderRepositoryV4.java
@@ -0,0 +1,40 @@
+package hello.advanced.app.v4;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV4 {
+
+    private final LogTrace trace;
+
+    public void save(TraceId traceId, String itemId) {
+
+        TraceStatus status = null;
+        try {
+
+            status = trace.begin("OrderRepository.save()");
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+            sleep(1000);
+
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    private static void sleep(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
